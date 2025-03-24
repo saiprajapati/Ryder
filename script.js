bd1b5e2ef82da75b75b1114a469d78f599a97464
Ananyaa-03 <ananya03trivedi@gmail.com>
2025-03-24 23:06:18 +0530

    initial commit

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    let header = document.querySelector("header");
-    if (window.scrollY > 50) {
-        header.style.background = "rgba(0, 0, 0, 0.5)"; // Slightly visible when scrolling
-    } else {
-        header.style.background = "rgba(0, 0, 0, 0)"; // Fully transparent at the top
-    }
-});
